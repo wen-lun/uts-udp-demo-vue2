3c42d4caf77eef0ed9b9b89a3363e2d632b8fbae
wenlun <1070586554@qq.com>
2024-01-11 17:42:08 +0800

    格式化代码

--- pages/server.vue
+++ pages/server.vue
@@ -1,84 +1,82 @@
 <template>
-	<view class="content">
-		<view class="btns">
-			<button v-if="!isStart" type="primary" size="mini" @click="onEnableClick">启动UDP服务</button>
-			<button v-else type="warn" size="mini" @click="onDisableClick">停止UDP服务</button>
-		</view>
-		<view class="status">{{ isStart ? 'UDP服务已启动' : 'UDP服务未启动' }}</view>
-		<view class="box">
-			<view v-for="item in messages" class="item">
-				<view class="info">
-					<text class="label">主机：{{ item.host }}</text>
-					<text class="label">端口：{{ item.port }}</text>
-				</view>
-				<view class="msg">消息：{{ item.msg }}</view>
-			</view>
-		</view>
-	</view>
+    <view class="content">
+        <view class="btns">
+            <button v-if="!isStart" type="primary" size="mini" @click="onEnableClick">启动UDP服务</button>
+            <button v-else type="warn" size="mini" @click="onDisableClick">停止UDP服务</button>
+        </view>
+        <view class="status">{{ isStart ? "UDP服务已启动" : "UDP服务未启动" }}</view>
+        <view class="box">
+            <view v-for="item in messages" class="item">
+                <view class="info">
+                    <text class="label">主机：{{ item.host }}</text>
+                    <text class="label">端口：{{ item.port }}</text>
+                </view>
+                <view class="msg">消息：{{ item.msg }}</view>
+            </view>
+        </view>
+    </view>
 </template>
 
 <script>
-	import {
-		UDPServer
-	} from '@/uni_modules/uts-udp';
+import { UDPServer } from "@/uni_modules/uts-udp";
 
-	let server = null;
+let server = null;
 
-	export default {
-		data() {
-			return {
-				isStart: false,
-				messages: []
-			}
-		},
-		methods: {
-			onEnableClick() {
-				if (this.isStart) return;
-				server = server || new UDPServer(7000);
-				server.listener(
-					(data) => {
-						this.messages.push(data);
-						// 回复客户端
-						server.send(`已收到消息：${data.msg}`, data.host, data.port);
-					},
-					(error) => {
-						console.error(error);
-						this.isStart = false;
-					},
-				);
-				this.isStart = true;
-			},
-			onDisableClick() {
-				server?.stop();
-				this.isStart = false;
-			}
-		}
-	}
+export default {
+    data() {
+        return {
+            isStart: false,
+            messages: []
+        };
+    },
+    methods: {
+        onEnableClick() {
+            if (this.isStart) return;
+            server = server || new UDPServer(7000);
+            server.listener(
+                (data) => {
+                    this.messages.push(data);
+                    // 回复客户端
+                    server.send(`已收到消息：${data.msg}`, data.host, data.port);
+                },
+                (error) => {
+                    console.error(error);
+                    this.isStart = false;
+                }
+            );
+            this.isStart = true;
+        },
+        onDisableClick() {
+            server?.stop();
+            this.isStart = false;
+        }
+    }
+};
 </script>
 
 <style lang="scss" scoped>
-	.content {
-		font-size: 15px;
-	}
+.content {
+    font-size: 15px;
+}
 
-	.btns {
-		padding: 10px;
-		text-align: right;
+.btns {
+    padding: 10px;
+    text-align: right;
 
-		button+button {
-			margin-left: 5px;
-		}
-	}
+    button + button {
+        margin-left: 5px;
+    }
+}
 
-	.box {
-		padding: 10px;
+.box {
+    padding: 10px;
 
-		.item {
-			padding: 10px;
+    .item {
+        padding: 10px;
 
-			.label+.label {
-				margin-left: 10px;
-			}
-		}
-	}
+        .label + .label {
+            margin-left: 10px;
+        }
+    }
+}
 </style>
